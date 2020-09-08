56be27e38a27b4128553906f943bd642f1500ef8
Germán <gapag.44@hotmail.com>
2020-09-08 11:34:15 -0500

    Se crea el proyecto en android studio y se crean las interfaces principales

--- app/src/main/java/com/example/pruebatecnica/ejercicio1.java
+++ app/src/main/java/com/example/pruebatecnica/ejercicio1.java
@@ -11,6 +11,6 @@ public class ejercicio1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ejercicio1);
-        toolbar.show
+
     }
 }
