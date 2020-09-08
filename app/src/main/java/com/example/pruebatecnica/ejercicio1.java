70676c79ee4a44c755395d6f9a86198b40244ebe
Germán <gapag.44@hotmail.com>
2020-09-08 15:19:52 -0500

    El primer ejercicio queda completo

--- app/src/main/java/com/example/pruebatecnica/ejercicio1.java
+++ app/src/main/java/com/example/pruebatecnica/ejercicio1.java
@@ -10,15 +10,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.example.pruebatecnica.includes.toolbar;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class ejercicio1 extends AppCompatActivity {
     private Button btn1;
-    private EditText edtPartida, edtRegreso;
+    private EditText edtPartida, edtRegreso,edtDistancia;
+    private String distancia;
+    private Date partida, regreso;
+    private int dias,precio, descuento,precioF;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,24 +37,55 @@ public class ejercicio1 extends AppCompatActivity {
         btn1= findViewById(R.id.btn1);
         edtPartida=findViewById(R.id.edtPartida);
         edtRegreso=findViewById(R.id.edtRegreso);
+        edtDistancia=findViewById(R.id.edtDistancia);
         edtPartida.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDatePickerDialog(1,"Fecha de Partida");
-
             }
         });
         edtRegreso.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDatePickerDialog(2,"Fecha de Regreso");
-
             }
         });
+
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                distancia=edtDistancia.getText().toString();
+                try {
+                    partida =deStringADate(edtPartida.getText().toString());
+                    regreso = deStringADate(edtRegreso.getText().toString());
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    dias = getDays(partida, regreso);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                if(!distancia.isEmpty() && !partida.toString().isEmpty() &&!regreso.toString().isEmpty()){
+                    if(regreso.after(partida)) {
+                        int distance = Integer.parseInt(distancia);
+                        if( dias>7 && distance>1000){
+                            precio=  (distance*35);
+                            descuento = (int) (precio*0.3);
+                            precioF= precio-descuento;
+                            Toast.makeText(ejercicio1.this, "El precio con descuento es de $"+precioF, Toast.LENGTH_SHORT).show();
+                        }else{
+                            precio=  (distance*35);
+                            Toast.makeText(ejercicio1.this, "El precio es $"+precio, Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(ejercicio1.this, "La fecha de regreso debe ser posterior a la de partida.", Toast.LENGTH_LONG).show();}
+                }else{
+                    Toast.makeText(ejercicio1.this, "Completa todos los campos.", Toast.LENGTH_SHORT).show();
+                }
+
 
             }
         });
@@ -82,4 +122,20 @@ public class ejercicio1 extends AppCompatActivity {
         newDialog.setMessage(msg);
         newDialog.show();
     }
+    public static Date deStringADate(String fechaString) throws ParseException {
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy");
+        Date fechaDate = formatoFecha.parse(fechaString);
+        return fechaDate;
+
+    }
+    public int getDays(Date inicio, Date fin) throws ParseException {
+        long MILLIS_PER_DAY = 24 * 60 * 60 * 1000;
+
+
+        long Inicio=inicio.getTime();
+        long Fin =fin.getTime();
+        long dife = (Fin - Inicio) / (MILLIS_PER_DAY);
+        return (int) dife;
+    }
+
 }
